cb2261e76ee5e6b84c31f8170ac75655877a8a98
0173916 <1257668543@qq.com>
2020-08-05 19:04:07 +0800

    severless开发

---
+++ wx-db/miniprogram/pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+const app = getApp()
+const db = wx.cloud.database();
+// mysql  table  row fields
+// mongodb(js) collection json Document 
+const productsCollection = 
+  db.collection('products');
+Page({
+  data: {
+    products: []
+  },
+  gotoDetail(event) {
+    productsCollection.doc(
+    event.currentTarget.dataset.id
+    )
+    .get()
+    .then(res => {
+      console.log(res)
+      this.setData({
+        products: res.data
+      })
+    })
+  },
+  onLoad() {
+    productsCollection
+      .get()
+      .then(res => {
+        // console.log(res);
+        this.setData({
+          products: res.data
+        })
+      })
+  }
+})
